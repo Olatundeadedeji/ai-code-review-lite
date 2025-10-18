7867227e7ab58e61ecdeaadc35d94061e9db0641
Olatunde Adedeji <olatundeadedeji32@gmail.com>
2025-10-18 04:44:04 -0400

    chore(ci): upload report.md artifact from sample run

--- examples/withbugs.py
+++ examples/withbugs.py
@@ -6,3 +6,10 @@ def insecure(cmd):
 
 def calc(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t):
     return a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t
+
+
+def insecure_more(cmd):
+    # BAD: shell=True triggers Bandit finding
+    import subprocess
+    subprocess.Popen(cmd, shell=True)
+    return True
